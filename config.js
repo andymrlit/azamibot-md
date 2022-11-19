0f3c779b91c9d53b27bf7e629bf5f4b2bd011bd8
andymrlit <andysebastien14@gmail.com>
2022-11-19 15:48:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-	['6285282250251'],
-	['79309965233'],
-	['6282337245566', 'Syahrul A.S', true]
+	['50941411147'],
+	['50941411147'],
+	['50941411147', 'andymrlit A.M.L', true]
 	// [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
